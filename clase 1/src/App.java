3a3b519ce1f3e29f25605dc0d8c787874de772de
unknown <barriospolocarlos@gmail.com>
2026-06-25 21:36:32 -0500

    commit 3

--- clase 1/src/App.java
+++ clase 1/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hola mundo");
         System.out.println("modificacion commit 2");
-    
+        System.out.println("modificacion commit 3");
     
     
     
