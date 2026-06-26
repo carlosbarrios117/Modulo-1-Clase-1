809c3d22afa9114ef6bc78a76ed2e687c67fe751
unknown <barriospolocarlos@gmail.com>
2026-06-25 22:25:48 -0500

    final commit

--- clase 1/src/App.java
+++ clase 1/src/App.java
@@ -1,13 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("hola mundo");
+
         System.out.println("modificacion commit 2");
         System.out.println("modificacion commit 3");
-    
-    
-    
-    
-    
+        System.out.println("prueba rama2");
+
     }
 
 
